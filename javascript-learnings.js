6d2aba30697e1af5c37e5b5cb7add36986c5ed8d
Tyler Fry <tyler.fry@shopify.com>
2019-10-11 22:21:16 -0400

    Initial push of all the stuff

---
+++ javascript-learnings.js
@@ -0,0 +1,27 @@
+// Javascript
+
+// {{}} placeholder
+
+// var = delcaring a variable
+// var {{name}} = {{value}}
+
+var hello = 'goodbye'
+
+// var is very loose
+// let is a bit stricter
+// const is very strict 
+
+// functions
+// function {{name}}({{arguments}}) {
+//   // do something
+//   return {{value}}
+// }
+
+function sayHello(name) {
+  return "Hello " + name 
+}
+
+function sayGoodbye(name) {
+  let lastName = "Dundas"
+  return "Goodbye " + name + " " + lastName
+}
